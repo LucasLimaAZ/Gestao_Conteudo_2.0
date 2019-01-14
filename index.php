c90a3fce3fb7d7570fe4a5719a31effcf3cfa938
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2019-01-14 17:41:42 -0200

    inicio

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="public/img/favicon.png" type="image/x-icon"/>
+    <link rel="shortcut icon" href="public/img/favicon.png" type="image/x-icon"/>
+    <link rel="shortcut icon" href="tema/img/favicon.png">
+    <link rel="apple-touch-icon" href="tema/img/icon57.png" sizes="57x57">
+    <link rel="apple-touch-icon" href="tema/img/icon72.png" sizes="72x72">
+    <link rel="apple-touch-icon" href="tema/img/icon76.png" sizes="76x76">
+    <link rel="apple-touch-icon" href="tema/img/icon114.png" sizes="114x114">
+    <link rel="apple-touch-icon" href="tema/img/icon120.png" sizes="120x120">
+    <link rel="apple-touch-icon" href="tema/img/icon144.png" sizes="144x144">
+    <link rel="apple-touch-icon" href="tema/img/icon152.png" sizes="152x152">
+    <link rel="apple-touch-icon" href="tema/img/icon180.png" sizes="180x180">
+    <link rel="stylesheet" href="tema/css/bootstrap.min.css">
+    <link rel="stylesheet" href="tema/css/plugins.css">
+    <link rel="stylesheet" href="tema/css/main.css">
+    <link rel="stylesheet" href="tema/css/themes.css">
+    <script src="tema/js/vendor/modernizr-3.3.1.min.js"></script>
+    <title>DOM - Creative Consulting</title>
+</head>
+<body>
+    <section class="site-content site-section-mini themed-background-muted border-bottom">
+        <div class="container">
+            <h2 class="site-heading h3 site-block">
+                <i class="fa fa-fw fa-chevron-right"></i> <strong>Passo 1 - Precisamos de Algumas Informações...</strong>
+            </h2>
+        </div>
+    </section>
+    <div class="col-lg-2"></div>
+    <section class="site-content site-section-mini themed-background-muted border-bottom col-lg-8">
+        <div class="form-group">
+            <label class="col-md-1 control-label" for="example-hf-email">Nome</label>
+            <div class="col-md-11">
+                <input type="text" id="nome" name="nome" class="form-control">
+            <span class="help-block">Por favor digite seu nome completo.</span>
+            </div>
+            <label class="col-md-1 control-label" for="example-hf-email">CPF / CNPJ</label>
+            <div class="col-md-11">
+                <input type="text" id="cpf" name="cpf" class="form-control">
+            <span class="help-block">Por favor digite seu cpf ou cnpj.</span>
+            </div>
+            <label class="col-md-1 control-label" for="example-hf-email">Necessidades:</label>
+            <div class="col-md-11">
+                <input type="checkbox" id="necessidades" name="necessidades" value="teste" class="form-control">
+                <input type="checkbox" id="necessidades" name="necessidades" value="teste" class="form-control">
+                <input type="checkbox" id="necessidades" name="necessidades" value="teste" class="form-control">
+                <input type="checkbox" id="necessidades" name="necessidades" value="teste" class="form-control">
+            <span class="help-block">Por favor digite seu cpf ou cnpj.</span>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
